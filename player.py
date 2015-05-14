9b2cea96fd9ff530e36012a2fdc876bcce3bf10e
Jake Irvine <me@jakeirvine.com>
2015-05-14 11:02:22 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,8 +1,10 @@
 import random
 
 class player(object):
-  def __init__(self, maxhp, minhp, maxmana, minmana, maxskill, minskill):
+  def __init__(self, name, race, maxhp, minhp, maxmana, minmana, maxskill, minskill):
     self.hp = random.randint(minhp, maxhp)
     self.mana = random.randint(minman, maxman)
     self.skill = random.randint(minskill, maxskill)
-  
+    self.name = name
+    self.race = race
+    
