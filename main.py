4197eff8462fdef11ed25d7409430d7cafbc3892
wizardsambolton <sam99bolton@gmail.com>
2015-05-14 21:34:55 +0100

    added basic login and register functions

--- main.py
+++ main.py
@@ -19,12 +19,21 @@ def new_player():
   theclass = input("Please type the letter of the class")
   switch theclass:
     case "R":
+def login():
+  username = input("Username: ")
+  password = input("password: ")
+def register():
+  player_username = input("enter a username: ")
+  player_password = input("enter a password: ")
+  
       
   
 startgame = input("If you are a new player type new player \nif you are an existing player type login\n ")
 
 if startgame.lower() == "new player":
-    new_player();  
+    new_player();
+if startgame.lower() == "login":
+    login(); 
   
   
   
