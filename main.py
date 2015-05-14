8af79f29d9f3672c5a5b90fcff53c57b6d66779a
sam bolton <sam99bolton@gmail.com>
2015-05-14 21:36:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import random
 import player
 #import :)
-# this is a test
+
 
 
 
 def new_player():
-  name = input("First, who are you?")
-  print ("Now, please select a class:")
+  print ("First of all, please select a class:")
   print ("\t[R]anger")
   print ("\t[W]arrior")
   print ("\t[M]age")
@@ -21,12 +20,21 @@ def new_player():
   switch theclass:
     case "R":
 		theplayer = Player(name, 200, 150, 100, 50, 100, 50) #ranger attributes
+def login():
+  username = input("Username: ")
+  password = input("password: ")
+def register():
+  player_username = input("enter a username: ")
+  player_password = input("enter a password: ")
+  
       
   
 startgame = input("If you are a new player type new player \nif you are an existing player type login\n ")
 
 if startgame.lower() == "new player":
-    new_player();  
+    new_player();
+if startgame.lower() == "login":
+    login(); 
   
   
   
