7c98f35e8aa8c3eb4fc162d09369c3d34a1ee6f1
Jake Irvine <me@jakeirvine.com>
2015-05-14 11:00:15 +0100

    Update player.py
    Added better randomizeation

--- player.py
+++ player.py
@@ -1,4 +1,8 @@
 import random
 
 class player(object):
+  def __init__(self, maxhp, minhp, maxmana, minmana, maxskill, minskill):
+    self.hp = random.randint(minhp, maxhp)
+    self.mana = random.randint(minman, maxman)
+    self.skill = random.randint(minskill, maxskill)
   
