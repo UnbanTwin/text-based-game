6adc91988cd24e25007d48eaa8abf065baabaadf
wizardsambolton <sam99bolton@gmail.com>
2015-05-14 16:52:11 +0100

    added start game functions

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import player
 #import :P
 
 
+
+
 def new_player():
   print ("Welcome to title here")
   print ("First of all, please select a class:")
   print ("\t[R]anger")
   print ("\t[W]arrior")
   print ("\t[M]age")
-  print ("\tR[o]uge")
+  print ("\t[O]Rouge")
   print ("\t[P]aladin")
   print ("\t[B]arbarian")
   print ("\t[C]leric")
   print ("\t[D]ruid")
   print ("\t[C]leric")
   
-  
+startgame = input("If you are a new player type new player \nif you are an existing player type login\n ")
+
+if startgame == "new player":
+    new_player();  
   
   
   
