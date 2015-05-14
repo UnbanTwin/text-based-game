6d05e017773bf30a0db3fd54681e4a96a24d5e09
Jake Irvine <me@jakeirvine.com>
2015-05-14 13:25:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import player
 
 
 def new_player():
-  print "w"
+  print "Welcome to title here"
+  print "First of all, please select a class:"
+  print "\t[R]anger"
+  print "\t[W]arrior"
+  print "\t[M]age"
+  print "\tR[o]uge"
+  print "\t[P]aladin"
+  print "\t[B]arbarian"
+  print "\t[C]leric"
+  print "\t[D]ruid"
+  print "\t[C]leric"
+  
+  
+  
+  
   
 
