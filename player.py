f8d1ff45fe726094af012b355a7d863ddfec17c8
Jake Irvine <me@jakeirvine.com>
2015-05-14 13:17:01 +0100

    Added CheckDead

--- player.py
+++ player.py
@@ -7,3 +7,15 @@ class player(object):
     self.skill = random.randint(minskill, maxskill)
     self.name = name
     self.race = race
+    self.gold = 200 + random.randint(50, 200)
+    self.inventory = []
+    
+  def check_dead(self):
+    if self.hp < 0:
+      print "You Died..."
+      self.inventory = []
+      self.gold = 0
+      return 0
+    else:
+      return self.hp
+    
