b9177682eb06fca158e3d20756129171a16535e5
Jake Irvine <me@jakeirvine.com>
2015-05-15 21:01:10 +0100

    Continues switch

--- main.py
+++ main.py
@@ -19,14 +19,17 @@ def new_player():
   print ("\t[C]leric")
   print ("\t[D]ruid")
   print ("\t[C]leric")
+  print ("\t[H]ealer")
+  print ("\t[K]Dark mage")
   theclass = input("Please type the letter of the class:\n>>")
   if theclass == "R":
       theplayer = Player(name, 200, 150, 100, 50, 100, 50) #ranger attributes
   elif theclass == "W":
       theplayer = Player(name, 250, 190, 70, 20, 125, 75)
-
   elif theclass == "M":
-      theplayer = Player(name, 250, 190, 70, 20, 100, 75)
+      theplayer = Player(name, 200, 225, 250, 200, 100, 75)
+  elif theclass == "O":
+      theplayer = Player(name, 200, 225, 250, 200, 100, 75)
   
  
 
