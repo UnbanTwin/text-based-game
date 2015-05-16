738ce6245531fb8988c5defb10398f58376cfca6
Jake Irvine <me@jakeirvine.com>
2015-05-16 10:06:33 +0100

    Fixed Switch

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import player
 import random
 
 class Player(object):
-  def __init__(self, name, race, maxhp, minhp, maxmana, minmana, maxskill, minskill):
+  def __init__(self, name, maxhp, minhp, maxmana, minmana, maxskill, minskill):
     self.hp = random.randint(minhp, maxhp)
-    self.mana = random.randint(minman, maxman)
+    self.mana = random.randint(minmana, maxmana)
     self.skill = random.randint(minskill, maxskill)
     self.name = name
-    self.race = race
+    #self.race = race
     self.gold = 200 + random.randint(50, 200)
     self.inventory = []
     
@@ -41,26 +41,29 @@ def new_player():
   print ("\t[H]ealer")
   print ("\t[K]Dark mage")
   theclass = input("Please type the letter of the class:\n>>")
-  if theclass == "R":
+  if theclass.upper() == "R":
       theplayer = Player(name, 200, 150, 100, 50, 100, 50) #ranger attributes
-  elif theclass == "W":
+  elif theclass.upper() == "W":
       theplayer = Player(name, 250, 190, 70, 20, 125, 75)
-  elif theclass == "M":
+  elif theclass.upper() == "M":
       theplayer = Player(name, 225, 200, 350, 200, 100, 75)
-  elif theclass == "O":
+  elif theclass.upper() == "O":
       theplayer = Player(name, 325, 300, 10, 0, 150, 100)
-  elif theclass == "P":
+  elif theclass.upper() == "P":
       theplayer = Player(name, 225, 100, 50, 100, 150, 100)
-  elif theclass == "B":
+  elif theclass.upper() == "B":
       theplayer = Player(name, 400, 250, 1, 0, 100, 50)
-  elif theclass == "C":
+  elif theclass.upper() == "C":
       theplayer = Player(name, 200, 190, 50, 100, 200, 190)
-  elif theclass == "D":
+  elif theclass.upper() == "D":
       theplayer = Player(name, 125, 100, 450, 400, 125, 100)
-  elif theclass == "H":
+  elif theclass.upper() == "H":
       theplayer = Player(name, 325, 300, 250, 200, 150, 100)
-  elif theclass == "K":
+  elif theclass.upper() == "K":
       theplayer = Player(name, 125, 100, 500, 450, 150, 100)
+  else:
+      print("Please select a letter of a class")
+      input("lll")
   
  
 
