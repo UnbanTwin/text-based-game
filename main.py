d6160e0e8621232536032674774a56581da54a18
wizardsambolton <sam99bolton@gmail.com>
2015-05-17 09:34:25 +0100

    bug fixes

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import player
 import random
 
 class Player(object):
-  def __init__(self, name, race, maxhp, minhp, maxmana, minmana, maxskill, minskill):
-    self.hp = random.randint(minhp, maxhp)
-    self.mana = random.randint(minman, maxman)
-    self.skill = random.randint(minskill, maxskill)
-    self.name = name
-    self.race = race
-    self.gold = 200 + random.randint(50, 200)
-    self.inventory = []
-    
-  def check_dead(self):
-    if self.hp < 0:
-      print ("You Died...")
+    def __init__(self, name, race, maxhp, minhp, maxmana, minmana, maxskill, minskill):
+      self.hp = random.randint(minhp, maxhp)
+      self.mana = random.randint(minman, maxman)
+      self.skill = random.randint(minskill, maxskill)
+      self.name = name
+      self.race = race
+      self.gold = 200 + random.randint(50, 200)
       self.inventory = []
-      self.gold = 0
-      return 0
-    else:
-      return self.hp
+    
+    def check_dead(self):
+      if self.hp < 0:
+        print ("You Died...")
+        self.inventory = []
+        self.gold = 0
+        return 0
+      else:
+        return self.hp
 
 #def startgame():
   #comment
@@ -80,9 +80,9 @@ def register():
 startgame = input("If you are a new player type new \nif you are an existing player type login\n ")
 
 if startgame.lower() == "new":
-    new_player();
+    new_player()
 if startgame.lower() == "login":
-    login(); 
+    login() 
   
   
   
